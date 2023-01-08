08cfa3d911bd84857081c6bbc2427b2b38958140
Efficient-Gazelle <dhsamrat27+github@gmail.com>
2023-01-08 21:02:51 +0545

    first day

---
+++ todo.js
@@ -0,0 +1,26 @@
+/*
+const buttonA = document.querySelector('#add'); //selects the html query using the id
+buttonA.onclick= () => {        //uses the function of onclick of buttonA 
+    console.log("hello");
+    const bigmouth = 'I\'ve got no right to take my place…'; //Escaping the inverted comma
+    console.log(bigmouth);
+}
+*/
+const l= document.getElementById('l')
+const task = document.getElementById('task');
+const button = document.querySelector("button"); //selects every button in the code
+const todolist= document.getElementById('list');
+function add(){
+    // Create a new input field
+    const newInput = document.createElement('input');
+    newInput.setAttribute('type', 'text');
+    newInput.setAttribute('placeholder', 'Your next task');
+    newInput.setAttribute('id', 'task');
+
+    // Create a new add button
+    const newButton = document.createElement('button');
+    newButton.setAttribute('id', 'add');
+    newButton.textContent = '+';
+    //button.appendChild(newb);
+}
+l.addEventListener("click",add); //runs the function add when the button is clicked
